73cfa4fb0b14426cec1a6676ad1962e0d36394f8
Muhammad Umair Maqsood <mianumair.maqsood@gmail.com>
2025-10-17 10:10:22 +0500

    Make Hero Section

--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -1,4 +1,20 @@
+import heroVideo from "../assets/hero.mp4";
+import "../components/banner.css";
 const Banner = () => {
-  return <section className="banner-section"></section>;
+  return (
+    <section className="banner-section">
+      <div className="hero-video">
+        <video src={heroVideo} autoPlay loop muted playsInline />
+      </div>
+      <div className="hero-content">
+        <h1>Shope Onlne make life easy</h1>
+        <p>
+          Lorem ipsum dolor, sit amet consectetur adipisicing elit. Tempora
+          dolorum voluptas accusantium, repellat reiciendis ad!
+        </p>
+        <button>Buy Now</button>
+      </div>
+    </section>
+  );
 };
 export default Banner;
